efa8d29bd44f4144d90b858b3d9dac0ecbc9c725
Methis-mac <138698717+Methis-mac@users.noreply.github.com>
2023-07-26 13:39:11 +0700

    user route

---
+++ routes/user.routes.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	"github.com/Methis-mac/golang-gorm-postgresql/controllers"
+	"github.com/Methis-mac/golang-gorm-postgresql/middleware"
+	"github.com/gin-gonic/gin"
+)
+
+type UserRouteController struct {
+	userController controllers.UserController
+}
+
+func NewUserRouteController(userController controllers.UserController) UserRouteController {
+	return UserRouteController{userController}
+}
+
+func (uc *UserRouteController) UserRoute(rc *gin.RouterGroup) {
+
+	router := rg.Group("users")
+	router.GET("/me", middleware.DeserializeUser(), uc.userController.GetMe)
+}
